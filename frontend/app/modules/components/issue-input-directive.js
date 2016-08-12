f15363114dbd7f2ad28a85104d5eb93c6ebd93d4
Nick Kramer <krames12@gmail.com>
2016-08-12 00:40:39 -0400

    label attribute added for flexibility. translates to label tag and placeholder for now.

--- frontend/app/modules/components/issue-input-directive.js
+++ frontend/app/modules/components/issue-input-directive.js
@@ -5,7 +5,10 @@
     .directive('issueInput', function(){
       return {
         restrict: 'E',
-        templateUrl: 'issue-input-directive.html'
+        templateUrl: 'issue-input-directive.html',
+        scope: {
+          label: '='
+        }
       };
     })
 })();
