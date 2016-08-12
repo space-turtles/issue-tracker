dc1ae0aa5664a3743b6390c50a047e786ad1078d
Nick Kramer <krames12@gmail.com>
2016-08-12 01:07:47 -0400

    controller added to directive.js. directive function also pulled out of .directive call

--- frontend/app/modules/components/issue-input-directive.js
+++ frontend/app/modules/components/issue-input-directive.js
@@ -2,13 +2,22 @@
   'use-strict';
   angular
     .module('issue-input')
-    .directive('issueInput', function(){
+    .directive('issueInput', Directive);
+    
+    function Directive(){
+      function Controller() {
+        var vm = this;
+      }
+
       return {
         restrict: 'E',
         templateUrl: 'issue-input-directive.html',
+        controller: [Controller],
+        controllerAs: 'vm',
+        bindToController: true,
         scope: {
           label: '='
         }
       };
-    })
+    }
 })();
