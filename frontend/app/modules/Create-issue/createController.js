e44623615779edb148ac3b38485d9ece6c38414a
Daniel Mata <damata1592@gmail.com>
2016-08-13 14:32:52 -0300

    last changes to the form

--- frontend/app/modules/Create-issue/createController.js
+++ frontend/app/modules/Create-issue/createController.js
@@ -2,7 +2,8 @@
   'use strict';
   angular
     .module('issue-tracker.Create-issue')
-    .controller('createController', ['$state', Controller]);
+    .controller('createController', ['$state', Controller], 
+							 ['$scope']);
   function Controller($state) {
    
   }
