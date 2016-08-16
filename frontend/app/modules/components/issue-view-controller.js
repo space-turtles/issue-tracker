9d8969f9635d8fd1a43396b0164d953ce01e8be7
Nick Kramer <krames12@gmail.com>
2016-08-16 15:05:55 +0000

    re-organized directive to make more sense, controller barebones added

---
+++ frontend/app/modules/components/issue-view-controller.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+    angular
+        .module('components')
+        .controller('viewController', Controller);
+    
+    function Controller() {
+        
+    };
+})();
