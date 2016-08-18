87937dee8ba59db1ca70b08b65271fd91fd92005
Daniel Mata <damata1592@gmail.com>
2016-08-18 20:27:53 -0300

    making a service call

--- frontend/app/modules/Create/issues-service.js
+++ frontend/app/modules/Create/issues-service.js
@@ -9,6 +9,11 @@
 		service.issuesList = []; 
 	  service.add = function (issue){
 		service.issuesList.push({id: service.issuesList.length, name: issue.name, description: issue.description});
+		service.makeCall = function() {
+      return $http.post(service.issuesList).then(function(response) {
+        return response;
+      });
+    };
 		
 	  };
 	
