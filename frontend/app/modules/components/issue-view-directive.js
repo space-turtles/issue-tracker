a34114cf95b73a816771d6a8717786e3d6fcb823
Nick Kramer <krames12@gmail.com>
2016-08-19 20:03:48 +0000

    each issue now has an issueId

--- frontend/app/modules/components/issue-view-directive.js
+++ frontend/app/modules/components/issue-view-directive.js
@@ -12,14 +12,17 @@
               'projectName': 'project',
               'issues': [
                 {
+                  "issueId": 0,
                   "issueName": "hasdf",
                   "isOpen": true
                 },
                 {
+                  "issueId": 1,
                   "issueName": "hello",
                   "isOpen": true
                 },
                 {
+                  "issueId": 2,
                   "issueName": "sadfwef",
                   "isOpen": true
                 }
